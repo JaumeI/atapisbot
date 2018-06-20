b96f7cab36e4d8798ba747f2e944a03b1421ba28
JaumeI <jaumei@gmail.com>
2018-06-20 19:26:48 +0200

    bug fixes

--- provaBot1.py
+++ provaBot1.py
@@ -22,11 +22,12 @@ class BotHandler:
 
     def get_last_update(self):
         get_result = self.get_updates()
+        
 
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)]
+            last_update = []
 
         return last_update
 
@@ -40,24 +41,27 @@ def main():
 
     while True:
         annoying_bot.get_updates(new_offset)
+        
        
         last_update = annoying_bot.get_last_update()
-        chat_type = last_update['message']['chat']['type']
+        if len(last_update)>0:
+            chat_type = last_update['message']['chat']['type']
 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['from']['first_name']
-        words_n = len(last_chat_text.split()) +1
-        insult_text="No, " + last_chat_name +",  I don't think you meant \""+last_chat_text +"\".\nIf you were more intelligent, you'd have sent a message with at least " + str(words_n) + " words. You're not welcome in this "
-        if chat_type == "private":
-            insult_text += "conversation"
-        elif chat_type == "group":
-            insult_text+="group"
-        
-        annoying_bot.send_message(last_chat_id,insult_text)
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['from']['first_name']
+            words_n = len(last_chat_text.split()) +1
+            insult_text="No, " + last_chat_name +",  I don't think you meant \""+last_chat_text +"\".\nIf you were more intelligent, you'd have sent a message with at least " + str(words_n) + " words. You're not welcome in this "
+            if chat_type == "private":
+                insult_text += "conversation"
+            elif chat_type == "group":
+                insult_text+="group"
+            
+            annoying_bot.send_message(last_chat_id,insult_text)
+            print (insult_text)
 
-        new_offset = last_update_id + 1
+            new_offset = last_update_id + 1
 
 if __name__ == '__main__':  
     try:
